ca884ecc30b7544798bb8e3fc8d2202eb1efb177
sdpalme <sdpalme@ea8bb76c-190c-11df-bbb2-637e8c2c76e3>
2010-03-23 20:26:40 +0000

    started mpi hw5

--- HW5/quadTree.c
+++ HW5/quadTree.c
@@ -178,103 +178,36 @@ quadTree *allocNewNode(int q, quadTree *tree)
 	return newNode;
 }
 
+quadTree *buildTree(quadTree *particles, int num_particles)
+{
+	quadTree *newTree = (quadTree *)malloc(sizeof(quadTree));
+	newTree->xBottomLeft = MIN_GRID;
+	newTree->yBottomLeft = MIN_GRID;
+	newTree->xTopRight = MAX_GRID;
+	newTree->yTopRight = MAX_GRID;
+	newTree->mass = 0.0;
+	newTree->topLeft = newTree->topRight = newTree->bottomLeft = newTree->bottomRight = 0;
+	
+	int i;
+	for(i = 0; i < num_particles, i++)
+	{
+		
+	}
+}
+
+
 quadTree *buildTree(quadTree *oldTree)
 {
 	quadTree *newTree = (quadTree *)malloc(sizeof(quadTree));
 
-	// TODO: decide if we can use the same outer bounding box
 	newTree->xBottomLeft = oldTree->xBottomLeft;
 	newTree->yBottomLeft = oldTree->yBottomLeft;
 	newTree->xTopRight = oldTree->xTopRight;
 	newTree->yTopRight = oldTree->yTopRight;
-//  	newTree->xBottomLeft = findxmin(oldTree);
-//  	newTree->yBottomLeft = findymin(oldTree);
-//  	newTree->xTopRight = findxmax(oldTree);
-//  	newTree->yTopRight = findymax(oldTree);
 	newTree->mass = 0.0;
 	newTree->topLeft = newTree->topRight = newTree->bottomLeft = newTree->bottomRight = 0;
 	buildTreeHelper(oldTree, newTree);
 	return newTree;
-}
-
-double findxmin(quadTree *tree)
-{
-	if(tree == 0)
-		return INT_MAX;
-	if(!hasChildren(tree))
-		return tree->xPosition;
-	double topLeftMin = findxmin(tree->topLeft);
-	double topRightMin = findxmin(tree->topRight);
-	double bottomLeftMin = findxmin(tree->bottomLeft);
-	double bottomRightMin = findxmin(tree->bottomRight);
-	double min = topLeftMin;
-	if(min > topRightMin)
-		min = topRightMin;
-	if(min > bottomLeftMin)
-		min = bottomLeftMin;
-	if(min > bottomRightMin)
-		min = bottomRightMin;
-	return min;
-}
-
-double findymin(quadTree *tree)
-{
-	if(tree == 0)
-		return INT_MAX;
-	if(!hasChildren(tree))
-		return tree->yPosition;
-	double topLeftMin = findymin(tree->topLeft);
-	double topRightMin = findymin(tree->topRight);
-	double bottomLeftMin = findymin(tree->bottomLeft);
-	double bottomRightMin = findymin(tree->bottomRight);
-	double min = topLeftMin;
-	if(min > topRightMin)
-		min = topRightMin;
-	if(min > bottomLeftMin)
-		min = bottomLeftMin;
-	if(min > bottomRightMin)
-		min = bottomRightMin;
-	return min;
-}
-
-double findymax(quadTree *tree)
-{
-	if(tree == 0)
-		return INT_MIN;
-	if(!hasChildren(tree))
-		return tree->yPosition;
-	double topLeftMin = findymax(tree->topLeft);
-	double topRightMin = findymax(tree->topRight);
-	double bottomLeftMin = findymax(tree->bottomLeft);
-	double bottomRightMin = findymax(tree->bottomRight);
-	double min = topLeftMin;
-	if(min < topRightMin)
-		min = topRightMin;
-	if(min < bottomLeftMin)
-		min = bottomLeftMin;
-	if(min < bottomRightMin)
-		min = bottomRightMin;
-	return min;
-}
-
-double findxmax(quadTree *tree)
-{
-	if(tree == 0)
-		return INT_MIN;
-	if(!hasChildren(tree))
-		return tree->yPosition;
-	double topLeftMin = findxmax(tree->topLeft);
-	double topRightMin = findxmax(tree->topRight);
-	double bottomLeftMin = findxmax(tree->bottomLeft);
-	double bottomRightMin = findxmax(tree->bottomRight);
-	double min = topLeftMin;
-	if(min < topRightMin)
-		min = topRightMin;
-	if(min < bottomLeftMin)
-		min = bottomLeftMin;
-	if(min < bottomRightMin)
-		min = bottomRightMin;
-	return min;
 }
 
 void buildTreeHelper(quadTree *oldTree, quadTree *newTree)
