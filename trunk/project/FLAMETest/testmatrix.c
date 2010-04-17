c4971a6573f3dab96b885aa1efab324552ace922
toomanyjoes <toomanyjoes@ea8bb76c-190c-11df-bbb2-637e8c2c76e3>
2010-04-17 00:55:48 +0000

    Flame matrix generator commit

---
+++ trunk/project/FLAMETest/testmatrix.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "FLAME.h"
+
+int main(int argc, char *argv[])
+{
+  int m, n, nfirst, nlast, ninc, i, irep,
+    nrepeats;
+
+  double
+    dtime,
+    dtime_best,
+    gflops,
+    diff;
+
+	struct timeval begin, end;
+
+
+  FLA_Obj
+    A, B, C;
+  
+  /* Initialize FLAME */
+  FLA_Init( );
+
+  /* Timing trials for matrix sizes n=nfirst to nlast in increments 
+     of ninc will be performed */
+  printf( "%% enter rows (n), and cols (m):" );
+  scanf( "%d%d", &n, &m );
+  printf( "%% %d %d\n", n, m );
+
+    /* Allocate space for the matrix and vectors */
+
+    FLA_Obj_create( FLA_DOUBLE, m, n, &A );
+    FLA_Obj_create( FLA_DOUBLE, m, n, &B );
+    FLA_Obj_create( FLA_DOUBLE, m, n, &C );
+  
+    /* Generate random matrices A and random vector x */
+    FLA_Random_matrix( A );
+    FLA_Random_matrix( B );
+   
+    FLA_Obj_show("A=[\n", A, "%lf", "\n];");
+    FLA_Obj_show("B=[\n", B, "%lf", "\n];");
+    
+    
+    gettimeofday(&begin, NULL);
+    
+    FLA_Gemm(FLA_NO_TRANSPOSE, FLA_NO_TRANSPOSE, FLA_ONE, A, B, FLA_ONE, C);
+    
+    gettimeofday(&end, NULL);
+    
+    FLA_Obj_show("C=[\n", C, "%lf", "\n];");
+    
+    double beginsec = (double)begin.tv_sec + ((double)begin.tv_usec/1000000.0);
+	 double endsec = (double)end.tv_sec + ((double)end.tv_usec/1000000.0);
+	 printf("Time: %lf\n",endsec-beginsec);
+	 
+    FLA_Obj_free(&A);
+	 FLA_Obj_free(&B);
+	 FLA_Obj_free(&C);
+
+ }
